c7b501707bc3b73488925bb932ba46eddc6564c1
Roham Pardakhtim <roham.p@gmail.com>
2018-04-24 17:44:08 -0700

    Added homework1

---
+++ BirthdayCardGenerator/BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Models.BirthdayCard confirmation)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Confirmation", confirmation);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BirthdayCardGenerator/BirthdayCardGenerator/Models/BirthdayCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class BirthdayCard
+    {
+        [Required (ErrorMessage ="Please enter From")]
+        public string From { get; set; }
+        [Required (ErrorMessage ="Please enter To")]
+        public string To { get; set; }
+        [Required (ErrorMessage ="Please enter Message")]
+        public string Message { get; set; }
+
+    }
+}
